3141c2c08d26d0a3ab0b99a39edba54399013810
Abhishek Kumar <166213438+abhiishhekk@users.noreply.github.com>
2024-07-03 15:33:17 +0000

    data types

---
+++ basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553 //cannot be changed once assigned
+
+//let and var both can be used to declare 
+//variable but let is used mostly because 
+//of scope problem
+let accountEmail = "abhioshek@gmail.com"
+var accountPassword = "12345"
+let accountState; //js consider it as undefined
+
+accountCity = "jaipur" //this is also a way to declare variable but not right way
+
+//to print a variable
+console.log(accountId);
+
+//can print all variable in a tabular form
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ basics/02_data_types.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as a newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+let name = "abhishek" //string
+let age = 29 //int
+let isloggedIn = false //bool
+
+//number --data type
+// bigint --to store large number
+//string =>""
+//boolean => true/false
+// null => standalone value
+//undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "abhishek"); //output string
+// for null datatype is object
+// for undefined is undefined itself
